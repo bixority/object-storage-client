8c21a3ae15d506f7f76bf4a127167e9c0708ef07
Olegs Korsaks <olegs@metisjean.com>
2026-03-06 01:19:09 +0200

    Code cleanup

--- tests/storage_ops.rs
+++ tests/storage_ops.rs
@@ -21,7 +21,10 @@ async fn test_object_lifecycle() -> anyhow::Result<()> {
 
     // 2. List it in bucket
     let list = client.list(&bucket_url).await?;
-    assert!(list.iter().any(|item| item == "test_file.txt"), "File should be in the list");
+    assert!(
+        list.iter().any(|item| item == "test_file.txt"),
+        "File should be in the list"
+    );
 
     // 3. Move it
     let moved_file_url = format!("{}/moved_test_file.txt", bucket_url);
@@ -29,15 +32,28 @@ async fn test_object_lifecycle() -> anyhow::Result<()> {
 
     // 4. List again to check the movement
     let list_after_move = client.list(&bucket_url).await?;
-    assert!(!list_after_move.iter().any(|item| item == "test_file.txt"), "Old file should NOT be in the list");
-    assert!(list_after_move.iter().any(|item| item == "moved_test_file.txt"), "Moved file should be in the list");
+    assert!(
+        !list_after_move.iter().any(|item| item == "test_file.txt"),
+        "Old file should NOT be in the list"
+    );
+    assert!(
+        list_after_move
+            .iter()
+            .any(|item| item == "moved_test_file.txt"),
+        "Moved file should be in the list"
+    );
 
     // 5. Delete
     client.delete(&moved_file_url).await?;
 
     // 6. List again to check the deletion
     let list_after_delete = client.list(&bucket_url).await?;
-    assert!(!list_after_delete.iter().any(|item| item == "moved_test_file.txt"), "Deleted file should NOT be in the list");
+    assert!(
+        !list_after_delete
+            .iter()
+            .any(|item| item == "moved_test_file.txt"),
+        "Deleted file should NOT be in the list"
+    );
 
     Ok(())
 }
